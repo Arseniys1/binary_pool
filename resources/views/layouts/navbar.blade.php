6d90f240b301ecce5d92c908d80d6dcaf73eedf3
Arseniys1 <thevalakas1@gmail.com>
2018-04-12 17:45:41 +0500

    fix

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg darkness mb-5">
-    <a class="navbar-brand" href="/">Binary pool</a>
+    <a class="navbar-brand" href="/">Binary Pool</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <i class="fa fa-bars" aria-hidden="true"></i>
